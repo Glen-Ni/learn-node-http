e11d54052329ce3ff8479062b78a0ef92eb115bb
小僵尸RUA <nijialun888@hotmail.com>
2022-01-03 16:26:52 +0800

    feat: 匹配文件；错误返回等；

--- index.ts
+++ index.ts
@@ -1,12 +1,42 @@
 import * as http from "http";
 import {IncomingMessage, ServerResponse} from "http";
+import * as path from "path";
+import * as url from "url";
+import * as fs from "fs";
 
 const server = http.createServer();
+const basicDir = path.join(__dirname, 'public');
 
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
   console.log('aaaaaaaa', response.constructor);
-  console.log(request.url);
-  console.log(request.method);
+  console.log('method', request.method);
+  const urlObj = url.parse(request.url);
+  let pathName = urlObj.pathname === '/' ? '/index.html' : urlObj.pathname;
+  const realPath = path.join(basicDir, pathName);
+  console.log('【路径】：', realPath);
+  fs.readFile(realPath, (error, data) => {
+    if (error) {
+      switch (error.errno) {
+        case -4058:
+          response.statusCode = 404;
+          fs.readFile(path.resolve(basicDir, '404.html'), (error2, data2) => {
+            response.end(data2);
+          });
+          break;
+        case -4068:
+          response.statusCode = 403;
+          response.end('no access');
+          break;
+        default:
+          response.statusCode = 500;
+          response.end('internal error');
+      }
+      return;
+    }
+    response.statusCode = 200;
+    response.end(data);
+  });
+
   const array = [];
   // request.on('data', (chunk) => {
   //   array.push(chunk);
@@ -16,8 +46,9 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
   //   console.log('body', body);
   //   response.end('aaaaa');
   // });
-  response.statusCode = 200;
-  response.end('hi');
+  // response.statusCode = 200;
+  // response.end('hi~');
+
 });
 
 server.listen('8888');
---
+++ public/main.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('#btn-how-dare-you');
+
+btn.addEventListener('click', () => {
+  alert('你牛逼好吧你牛逼');
+});
